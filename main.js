51f7bab21d21eaba85b5a63b19f7f1ce17eff47d
Oleksii_Deiko <deyko@dlit.dp.ua>
2022-02-21 21:07:29 +0200

    Add basic logic Add interactive and noninteractive modes

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
 const fs = require("fs");
+const readline = require("readline");
 
-const interactiveMode = () => {};
+const interactiveMode = () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-const noninteractiveMode = () => {};
+  rl.question("a =  ", function (a) {
+    rl.question("b =  ", function (b) {
+      rl.question("c = ", function (c) {
+        console.log(a, b, c);
+        rl.close();
+      });
+    });
+  });
+
+  rl.on("close", function () {
+    console.log("\nBYE BYE !!!");
+    process.exit(0);
+  });
+};
+
+const noninteractiveMode = (filename) => {
+  fs.readFile(filename, "utf8", function (err, data) {
+    if (err) throw err;
+    console.log(data);
+  });
+};
 
 if (process.argv.length < 3) {
   console.log("interactive mode");
   interactiveMode();
-  process.exit(1);
+} else {
+  let filename = process.argv[2];
+  console.log("noninteractive mode");
+  noninteractiveMode(filename);
 }
-
-console.log("noninteractive mode");
-noninteractiveMode();
